f2c9dbf6c533470e03b68ca5603e79e3185ec264
Bendee01 <123626028+Bendee01@users.noreply.github.com>
2024-04-03 23:06:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Configolás","Discord BOT programozás","Felújítás","Admin munka","❗bendehpreview.hu❗"];
+const statusMessages = ["bendehpreview.hu"];
 
 
 let currentIndex = 0;
@@ -73,7 +73,7 @@ function updateStatusAndSendMessages() {
   const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
 
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    activities: [{ name: currentStatus, type: ActivityType.Watching}],
     status: 'dnd',
   });
 
